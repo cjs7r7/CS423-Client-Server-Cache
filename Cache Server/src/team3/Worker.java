56d2ab8ec6dc094ba9e403bba0f0825aa44a1d8a
Cjsy3c <CodySmith@umkc.edu>
2017-05-04 17:44:43 -0500

    Added possibility of getting multiple pages

--- Cache Server/src/team3/Worker.java
+++ Cache Server/src/team3/Worker.java
@@ -7,56 +7,88 @@ import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.HashMap;
-import java.util.List;
 
 public class Worker implements Runnable {
 
 	private Socket socket;
-	private static String webServer = "127.0.0.1:8080"; // port 8080
+	private static final String webServer = "127.0.0.1:8080"; // port 8080
 
-	private static List<String> page;
+	private Repository database = Repository.getInstance();
 
 	public Worker(Socket socket) {
 		this.socket = socket;
+
 	}
 
 	@Override
 	public void run() {
 
 		try {
-
+			
+			// pull the headers from the request
 			System.out.println(Thread.currentThread().getName() + " - Calling getHeaders");
 			BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 			HashMap<String, String> hmap = Http.getHeaders(reader);
+			
+			System.out.println("done with headers");
 
+			// display headers
+			System.out.println(hmap.keySet().toString());
+			
+			System.out.println("augsdfuhasd");
+			
 			for (String key : hmap.keySet()) {
 				System.out.println("key: " + key);
 				System.out.println("value: " + hmap.get(key));
 			}
-
-			if (page == null || page.isEmpty()) {
-				System.out.println(Thread.currentThread().getName() + " - Calling getConnection");
-				page = Http.getConnection(webServer);
+			
+			// pull specific file requested
+			String uri;
+			if(hmap.isEmpty()){
+				uri = "/";
 			}
 			else{
+				uri = hmap.get("request").split(" ")[1];
+			}
+			System.out.println("Requested: '"+uri+"'");
+			
+			// check cache for page uri
+			if( database.hasKey(uri) ){
 				System.out.println(Thread.currentThread().getName() + " - Pulling from cache");
 			}
-
+			else {
+				
+				System.out.println(Thread.currentThread().getName() + " - Calling getConnection");
+				
+				String value= Http.getConnection(webServer + uri);
+				database.addEntry(uri, value);
+			}
+			
+			//opening output
 			System.out.println(Thread.currentThread().getName() + " - starting output stream");
 			PrintWriter writer = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()));
 
-			
+			// setting the response headers
 			System.out.println(Thread.currentThread().getName() + " - Setting Output Headers");
 			Http.setResponseHeaders(writer);
-
+			
+			// returning file
 			System.out.println(Thread.currentThread().getName() + " - Setting Output Body");
-			for (String line : page) {
-				writer.println(line);
-			}
+			
+			writer.println(database.getEntry(uri));
+			
 
+			writer.flush();
 			writer.close();
+			reader.close();
 			socket.close();
+			
+			System.out.println("Saved to database:");
 
+			for(String i : database.getKeys()){
+				System.out.println(i);
+			}
+			
 			System.out.println(Thread.currentThread().getName() + " - Done");
 
 		} catch (IOException e) {
